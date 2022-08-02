32119c9f1c4238b9aea54ba3cb27dbf8ccbda569
Mert Tecimen <merttecimen@yahoo.com.tr>
2022-08-02 09:51:10 +0300

    Updated README file.

--- Card Filpper (PokeAPI).swiftpm/String + Extenstions.swift
+++ Card Filpper (PokeAPI).swiftpm/String + Extenstions.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
